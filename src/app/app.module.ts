8dfbce54c45208b65f46b9f660fe42868f53b09f
Jonathan Ore <jore@purdue.edu>
2020-06-14 16:52:49 -0400

    Initial project commit.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    // Angular
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+
+    // App
+    AppRoutingModule,
+
+    // Shared
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
